93b46a0ab2a2f3e52b8bb40e151763fb6de59b3a
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-07-28 02:03:16 +0300

    added JWT token with role configured user model

--- code-editor-backend/routes/api.php
+++ code-editor-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -26,6 +27,8 @@ Route::prefix('user')->group(function () {
 
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+    Route::post('login',[\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/register',[\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/logout',[\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/refresh',[\App\Http\Controllers\AuthController::class, 'refresh']);
