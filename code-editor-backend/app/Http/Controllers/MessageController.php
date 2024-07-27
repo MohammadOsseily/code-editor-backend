95efb93f8bca7b34631bf06eb31171c1914b7783
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-07-27 17:15:04 +0300

    added Message controller fixed code submission typo

--- code-editor-backend/app/Http/Controllers/MessageController.php
+++ code-editor-backend/app/Http/Controllers/MessageController.php
@@ -1,10 +1,32 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Models\Chat;
+use App\Models\Message;
 use Illuminate\Http\Request;
-
 class MessageController extends Controller
 {
-    //
+    public function get(Request $request, $chat_id)
+    {
+        $messages = Message::where('chat_id', $chat_id)->get();
+        return response()->json($messages, 200);
+    }
+    public function createMessage(Request $request, $chat_id)
+    {
+        $request->validate([
+            'sender_id' => 'required|exists:users,id',
+            'message' => 'required|string',
+        ]);
+        $chat = Chat::find($chat_id);
+        if (!$chat) {
+            return response()->json(['error' => 'Chat not found'], 404);
+        }
+        $message = new Message();
+        $message->chat_id = $chat_id;
+        $message->sender_id = $request->sender_id;
+        $message->message = $request->message;
+        $message->save();
+        return response()->json([
+            'message' => $message,
+        ], 201);
+    }
 }
--- code-editor-backend/app/Models/Code_Submission.php
+++ code-editor-backend/app/Models/Code_Submission.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Code_submissin extends Model
+class Code_submission extends Model
 {
     use HasFactory;
     public function user()
