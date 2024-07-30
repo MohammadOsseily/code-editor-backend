77abb8e06cf65e235b5896b1def835c9a37105cc
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-07-30 12:29:24 +0300

    added search for the user added atribute name for the codesubmission table

--- code-editor-backend/app/Http/Controllers/UserController.php
+++ code-editor-backend/app/Http/Controllers/UserController.php
@@ -96,4 +96,20 @@ class UserController extends Controller
 
         return response()->json(['message' => 'User deleted successfully']);
     }
+    public function search(Request $request)
+    {
+        $request->validate([
+            'query' => 'required|string|max:255',
+        ]);
+
+        $query = $request->input('query');
+
+        $users = User::where('name', 'LIKE', "%{$query}%")
+                    ->orWhere('email', 'LIKE', "%{$query}%")
+                    ->get();
+
+        return response()->json([
+            'users' => $users,
+        ]);
+    }
 }
