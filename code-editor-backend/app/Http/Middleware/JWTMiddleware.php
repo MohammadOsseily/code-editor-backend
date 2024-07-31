b445f4d57dbe913b8343206ad9e87152e03a9df9
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-07-31 17:11:38 +0300

    add jwt

--- code-editor-backend/app/Http/Middleware/JWTMiddleware.php
+++ code-editor-backend/app/Http/Middleware/JWTMiddleware.php
@@ -3,18 +3,32 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Exception;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class JWTMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token is Invalid'], 401);
+            } else if ($e instanceof \PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is Expired'], 401);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found'], 401);
+            }
+        }
+
         return $next($request);
     }
 }
